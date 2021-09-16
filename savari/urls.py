edceb2cc60e5e1d6cf1b19865254224c352bc96d
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-09-16 12:34:37 +0530

    Add : Global Home Page

--- savari/urls.py
+++ savari/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from userAccount import views
+from django.urls.conf import include
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/',views.driverSignup)
+    path('user/',include('userAccount.urls')),
+    path('',views.home)
 ]
