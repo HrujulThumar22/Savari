f37bf4e2ab43e16e4e30107c017b7a46eb7b2a31
Gaurav Vaghasiya <gvaghasiya02@gmail.com>
2021-09-29 09:32:36 +0530

    vehicle added in startjourney

--- driver/forms.py
+++ driver/forms.py
@@ -9,5 +9,5 @@ class TripCreationForm(ModelForm):
             visible.field.widget.attrs['class'] = 'form-control'
     class Meta:
         model=DriverTrip
-        fields=['Starting_City','Destination_City','Trip_Starting_Date','Trip_Starting_Time','Trip_Ending_Date','Trip_Ending_Time']
+        fields=['Starting_City','Destination_City','Trip_Starting_Date','Trip_Starting_Time','Trip_Ending_Date','Trip_Ending_Time','Vehicle_Name','Vehicle_Number']
         widgets={'Trip_Starting_Date':widgets.DateInput(attrs={'type':'date'}),'Trip_Ending_Date':widgets.DateInput(attrs={'type':'date'}),'Trip_Starting_Time':widgets.TimeInput(attrs={'type':'time'}),'Trip_Ending_Time':widgets.TimeInput(attrs={'type':'time'})}
--- driver/models.py
+++ driver/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.fields import DateTimeField
+from django.core.validators import RegexValidator
 # Create your models here.
 class DriverTrip(models.Model):
     Starting_City=models.ForeignKey("city.City",on_delete=models.CASCADE,related_name='start_city')
@@ -8,6 +9,9 @@ class DriverTrip(models.Model):
     Trip_Starting_Time=models.TimeField()
     Trip_Ending_Date=models.DateField()
     Trip_Ending_Time=models.TimeField()
+    Vehicle_Name=models.CharField(max_length=50)
+    number_regex=RegexValidator(regex=r'^[A-Z]{2}[-][0-9]{1,2}[-][A-Z]{1,2}[-][0-9]{3,4}$', message="Vehicle number must be entered in the format: MH-03-C-3843")
+    Vehicle_Number=models.CharField(validators=[number_regex], max_length=17, blank=True)
     Driver=models.ForeignKey("userAccount.User", on_delete=models.CASCADE)
     isTripStarted=models.BooleanField(default=False)
 
