e404d2d555d757d29c70e718d045fab4633be67d
HrujulThumar22 <hrujul.a.thumar@gmail.com>
2021-10-16 10:25:26 +0530

    Create Driver Home Page and display trips

--- driver/urls.py
+++ driver/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('',views.Home,name="driver_home"),
     path('start/',views.StartJourney,name="driver_start"),
 ]
--- driver/views.py
+++ driver/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from driver.forms import TripCreationForm
 from django.shortcuts import get_object_or_404, redirect, render
 from userAccount.models import User
+from .models import DriverTrip
 # Create your views here.
 @login_required
 @allowed_users(allowed_role=['driver'])
@@ -14,9 +15,18 @@ def StartJourney(request):
             trip = form.save(commit=False)
             trip.Driver=user
             trip.save()
-            return redirect('home')
+            return redirect('driver_home')
         else:
             return redirect('driver_start')
     form=TripCreationForm()
     context={'form':form}
     return render(request,'driver/startJourney.html',context)
+
+def Home(request):
+    trips=DriverTrip.objects.filter(Driver_id=request.user.id)
+    if trips.exists():
+        context={'trips':trips}
+    else:
+        context={'trips':False}
+    return render(request,'driver/home.html',context)
+
